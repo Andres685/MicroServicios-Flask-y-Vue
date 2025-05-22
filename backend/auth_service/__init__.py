80615f7d4a1c5e7ac5eab6da52135e5a93f62442
Andres Guzman <guzmanandres685gmail.com>
2025-05-22 17:36:33 -0500

    autenticacion

--- backend/auth_service/__init__.py
+++ backend/auth_service/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from flask_cors import CORS
-from .models import db
-from .routes import routes
-from .config import Config
+from models import db
+from routes import routes
+from config import Config
 
 def create_app():
     app = Flask(__name__)
